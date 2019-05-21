5b50e251b7269d195c1416241527424d9bc0443d
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-05-21 18:23:47 +0100

    Failing test if the number is 15

--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -6,11 +6,15 @@ describe 'fizzbuzz' do
     expect(1.fizzbuzz).to eq 1
   end
 
-  it 'returns fizz if the number is a multiple of 3' do
+  it 'returns fizz if the number is 3' do
     expect(3.fizzbuzz).to eq "Fizz"
   end
 
-  it 'return buzz if the number is a multiple of 5' do
+  it 'return buzz if the number is 5' do
     expect(5.fizzbuzz).to eq "Buzz"
+  end
+
+  it 'returns fizzbuzz if the number is 15' do
+    expect(15.fizzbuzz).to eq "FizzBuzz"
   end
 end
