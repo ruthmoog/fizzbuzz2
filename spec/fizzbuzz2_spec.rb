e3898a57b81355982e6ca167f67b7dff401aecb4
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-05-21 18:05:27 +0100

    failing test

--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -5,4 +5,8 @@ describe 'fizzbuzz' do
   it 'returns a number when given a number'do
     expect(1.fizzbuzz).to eq 1
   end
+
+  it 'returns fizz if the number is a multiple of 3' do
+    expect(3.fizzbuzz).to eq "Fizz" 
+  end
 end
